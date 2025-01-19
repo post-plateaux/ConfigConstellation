f0f1928c03bbcd49435d89a059a4d6feb5e3caed
post-plateaux <robert.d.montalvowest@gmail.com>
2025-01-20 02:58:28 +0400

    feat: Create start-win11.sh script for launching Windows 11 VM

--- ~/code/arch_config/scripts/start-win11.sh
+++ ~/code/arch_config/scripts/start-win11.sh
@@ -2,3 +2,7 @@
 nohup virsh start win11 > /dev/null 2>&1 &
 sleep 2
 nohup virt-viewer win11 > /dev/null 2>&1 &
+#!/bin/bash
+nohup virsh start win11 > /dev/null 2>&1 &
+sleep 2
+nohup virt-viewer win11 > /dev/null 2>&1 &
