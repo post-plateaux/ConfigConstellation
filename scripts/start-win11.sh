4dcd3552be61554b07b59fc3b10352fc717436f1
post-plateaux <robert.d.montalvowest@gmail.com>
2025-01-20 04:54:22 +0400

    refactor: Remove libnotify notification from VM startup script

--- scripts/start-win11.sh
+++ scripts/start-win11.sh
@@ -4,7 +4,6 @@
 if virsh domstate win11 2>/dev/null | grep -q "running"; then
     # VM is running, shut it down gracefully
     virsh shutdown win11
-    notify-send "Windows VM" "Shutting down..."
 else
     # Switch to workspace 99 first
     hyprctl dispatch workspace 99
