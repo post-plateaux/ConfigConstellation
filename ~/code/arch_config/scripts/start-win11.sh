8154353cd249877955be94fe1a4dc46e12bfd18e
post-plateaux <robert.d.montalvowest@gmail.com>
2025-01-20 23:09:39 +0400

    moved wallpaper to this repo

--- ~/code/arch_config/scripts/start-win11.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-nohup virsh start win11 > /dev/null 2>&1 &
-sleep 2
-nohup virt-viewer win11 > /dev/null 2>&1 &
-#!/bin/bash
-nohup virsh start win11 > /dev/null 2>&1 &
-sleep 2
-nohup virt-viewer win11 > /dev/null 2>&1 &
