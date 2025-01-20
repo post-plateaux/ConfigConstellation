247e3674b28f0a05600e252b62a2dff85c8bd4d0
post-plateaux <robert.d.montalvowest@gmail.com>
2025-01-20 19:36:23 +0400

    fix: Remove duplicate content in get-tlp-state.sh script

--- dotfiles/waybar/scripts/get-tlp-state.sh
+++ dotfiles/waybar/scripts/get-tlp-state.sh
@@ -7,12 +7,3 @@ elif [ "$state" = "powersave" ]; then
 else
     echo "?"
 fi
-#!/bin/bash
-state=$(sudo tlp-stat -p | grep "scaling_governor" | awk '{print $5}')
-if [ "$state" = "performance" ]; then
-    echo "󰓅"  # Icon for performance mode
-elif [ "$state" = "powersave" ]; then
-    echo "󰾆"  # Icon for power save mode
-else
-    echo "?"
-fi
