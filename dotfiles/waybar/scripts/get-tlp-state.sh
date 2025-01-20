b442edcb08060f8bba007058100a571137ede00c
post-plateaux <robert.d.montalvowest@gmail.com>
2025-01-20 19:45:35 +0400

    style: Change power state display to "BATT" in TLP script

--- dotfiles/waybar/scripts/get-tlp-state.sh
+++ dotfiles/waybar/scripts/get-tlp-state.sh
@@ -2,6 +2,6 @@
 state=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor)
 case "$state" in
     "performance") echo "PERF" ;;
-    "powersave") echo "POWER" ;;
+    "powersave") echo "BATT" ;;
     *) echo "?" ;;
 esac
