161926ca38f4db74bd035099a441801d589969da
post-plateaux <robert.d.montalvowest@gmail.com>
2025-01-20 19:44:40 +0400

    feat: Update Waybar script to display text instead of icons

--- dotfiles/waybar/scripts/get-tlp-state.sh
+++ dotfiles/waybar/scripts/get-tlp-state.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 state=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor)
 case "$state" in
-    "performance") echo "󰓅" ;;
-    "powersave") echo "󰾆" ;;
+    "performance") echo "PERF" ;;
+    "powersave") echo "POWER" ;;
     *) echo "?" ;;
 esac
