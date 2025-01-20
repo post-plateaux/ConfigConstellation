31bb40ee18767c101b551faed52491a85f115a7c
post-plateaux <robert.d.montalvowest@gmail.com>
2025-01-20 19:35:28 +0400

    refactor: Update Waybar TLP state script to correctly detect power mode

--- dotfiles/waybar/scripts/get-tlp-state.sh
+++ dotfiles/waybar/scripts/get-tlp-state.sh
@@ -7,3 +7,12 @@ elif [ "$state" = "powersave" ]; then
 else
     echo "?"
 fi
+#!/bin/bash
+state=$(sudo tlp-stat -p | grep "scaling_governor" | awk '{print $5}')
+if [ "$state" = "performance" ]; then
+    echo "󰓅"  # Icon for performance mode
+elif [ "$state" = "powersave" ]; then
+    echo "󰾆"  # Icon for power save mode
+else
+    echo "?"
+fi
